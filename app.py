4a38d9836622155426ecde6c2c02de428dc1fe8f
swkang99 <kseongwoo99@gmail.com>
2023-12-03 02:57:03 +0900

    [feat] DB에 센서값 저장

--- app.py
+++ app.py
@@ -3,12 +3,57 @@ import cv2
 import numpy as np
 from datetime import datetime
 import torch
+from flask_sqlalchemy import SQLAlchemy
 import pymysql
 pymysql.install_as_MySQLdb()
 
+
+
 app = Flask(__name__)
 
-from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy import Column, Integer, String, Date, DECIMAL, Enum, ForeignKey, Time
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(255))
+    password = Column(String(266))
+    user_level = Column(Enum('실버', '골드', '다이아몬드'))
+    eco_points = Column(Integer)
+    
+
+class TrashBin(Base):
+    __tablename__ = 'trash_bin'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    state = Column(String(255))
+    trash_type = Column(String(255))
+    disposal_date = Column(Date)
+    disposal_amount = Column(DECIMAL(10,2))
+
+    user = relationship('Users', back_populates='trash_bins')
+
+    
+class TrashBinCapacity(Base):
+    __tablename__ = 'trash_bin_capacity'
+
+    id = Column(Integer, primary_key=True)
+    trash_bin_id = Column(Integer, ForeignKey('trash_bin.id'))
+    check_date = Column(Date)
+    check_time = Column(Time)
+    capacity = Column(DECIMAL(10,2))
+
+    trash_bin = relationship('TrashBin', back_populates='capacities')
+
+
+Users.trash_bins = relationship('TrashBin', order_by=TrashBin.id, back_populates='user')
+TrashBin.capacities = relationship('TrashBinCapacity', order_by=TrashBinCapacity.id, back_populates='trash_bin')
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:0000@localhost/autotrash'
 db = SQLAlchemy(app)
 
@@ -56,6 +101,39 @@ def save(cls, conf, pred_time):
     db.session.commit()
     print("Save Object Detection Result to DB")
 
+
+@app.route('/ultwave', methods=['GET'])
+def save_ult_wave():
+    dist = request.args['dist']
+    print(dist)
+    cls = request.args['cls']
+    
+    # users 테이블에서 id가 2인 user 검색
+    user = db.session.query(Users).get(2)
+    
+    # 해당 user와 연결된 trash_bin 테이블에서 trash_bin_id 검색
+    for trash_bin in user.trash_bins:
+        print(cls)
+        if trash_bin.trash_type in cls: 
+            # 해당 trash_bin_id와 연결된 모든 trash_bin_capacity 테이블의 레코드에 dist 값 저장
+            for capacity in trash_bin.capacities:
+                capacity.capacity = dist
+            
+    db.session.commit()
+
+    return '저장 완료'
+
+
+@app.route('/ecop', methods=['GET'])
+def plus_ecopoint():
+    
+    # users 테이블에서 id가 2인 user 검색
+    user = db.session.query(Users).get(2)
+    
+    user.eco_points += 10
+    
+    db.session.commit()
+    return 'eco_points 증가 완료'
 
 @app.route('/', methods=['GET'])
 def test():
